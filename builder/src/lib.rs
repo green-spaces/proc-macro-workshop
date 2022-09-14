f5d4283a5f84523669dc05b1b4eb8dd7702c7d6e
Matthew Taylor <mwtaylor902@gmail.com>
2022-09-14 08:31:55 -0400

    Builder passes test 2

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,35 @@
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
+use quote::{quote, format_ident};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input_tree = parse_macro_input!(input as DeriveInput);
+    // eprintln!("DeriveInput: {:#?}", input_tree);
 
-    unimplemented!()
+    let source_ident = input_tree.ident;
+    let builder_ident = format_ident!("{}Builder", source_ident);
+
+    let tokens = quote! {
+
+        pub struct #builder_ident {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+
+        impl #source_ident {
+            pub fn builder() -> #builder_ident {
+                #builder_ident {
+                    executable: None,
+                    args: None,
+                    env: None, 
+                    current_dir: None
+                }
+            }
+        }
+    };
+
+    tokens.into()
 }
